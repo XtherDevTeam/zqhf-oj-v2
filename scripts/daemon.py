a25d5cb95e7ce255d7cf9eaaf17b4113bb5240e7
xiaokang00010 <zhouyongkang4321@gmail.com>
2022-03-05 02:40:52 +0000

    Added daemon.py

--- scripts/daemon.py
+++ scripts/daemon.py
@@ -1,42 +1,56 @@
 #!/bin/python3
 import hashlib
-import os, json
+import os
+import signal
+import subprocess
 import sys
 import time
+
 
 def do_start(program: str):
     while os.access(hashlib.md5(program.encode('utf-8')).hexdigest() + '.lock', os.F_OK):
         print("Resource locked.")
         time.sleep(1)
+
     pid = os.fork()
     if pid == 0:
+        proc = subprocess.Popen(program, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+
         with open(hashlib.md5(program.encode('utf-8')).hexdigest() + '.lock', 'w+') as file:
             file.write(str(os.getpid()))
-        
-        os.system(program)
+
+        proc.communicate()
         os.remove(hashlib.md5(program.encode('utf-8')).hexdigest() + '.lock')
     else:
-        sys.exit(0)
-    
+        time.sleep(1)
+
+
 def do_stop(program: str):
     try:
         with open(hashlib.md5(program.encode('utf-8')).hexdigest() + '.lock', 'r+') as file:
-            os.kill(int(file.read()), 15)
+            os.kill(int(file.read()), signal.SIGTERM)
         os.remove(hashlib.md5(program.encode('utf-8')).hexdigest() + '.lock')
         return True
     except Exception as e:
         print(str(e))
         return False
-    
+
+
 if sys.argv[1] == 'start':
     if sys.argv[2] == 'api':
         do_start('cd ../backend/src && gunicorn -w 4 -b 0.0.0.0:9013 app:app')
     elif sys.argv[2] == 'judge':
-        do_start('cd ../judge && python3 judgeServer.py')
-        
-    
+        do_start('cd ../judge && ./judgeServer.py')
+    elif sys.argv[2] == 'web':
+        os.system('service nginx start')
+
 if sys.argv[1] == 'stop':
-    if sys.argv[2] == 'api':
-        do_stop('cd ../backend/src && gunicorn -w 4 -b 0.0.0.0:9013 app:app')
-    elif sys.argv[2] == 'judge':
-        do_stop('cd ../judge && python3 judgeServer.py')
+    os.system('killall gunicorn && killall judgeServer.py')
+
+if sys.argv[1] == 'install':
+    if sys.argv[2] == 'page':
+        os.system(
+            'mkdir -p /var/www/html/project; cd ../frontend; make build; cp -r -v ./dist/* /var/www/html/project/;exit')
+    if sys.argv[2] == 'nginx':
+        os.system('cp templates/nginx_config.conf /etc/nginx/conf.d/default.config')

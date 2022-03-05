ec665f39546b8a24c560e60957809c6cc53227c6
xiaokang00010 <zhouyongkang4321@gmail.com>
2022-03-05 05:03:25 +0000

    Added gitignore file

--- judge/tmp/temp.cpp
+++ judge/tmp/temp.cpp
@@ -1,115 +1,53 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <queue>
-using namespace std;
 
-struct node {
-    int x, y, z, step;
+int n;
+int map[15][15], total = 0;
+int direction[8][2] = {
+    { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 }
 };
 
-int d[4][2] = { 0, 1, 1, 0, 0, -1, -1, 0 };
-int vis[11][11][3];
-char s[11][11][3];
-int sx, sy, sz, ex, ey, ez;
-int flag;
-int m, n, tt, t;
+bool visited[15][15];
 
-void bfs(int x, int y, int z, int step) {
-    memset(vis, 0, sizeof vis);
+bool checkIndex(int x, int y) { return x >= 0 && x < n && y >= 0 && y < n; }
 
-    node e1{ x, y, z, 0 }, e2;
-    e1.x = x;
-    e1.y = y;
-    e1.z = z;
-    e1.step = 0;
+void search(int x, int y) {
+    if (x == 0 && y == n - 1) {
+        total += 1;
+        return;
+    }
 
-    vis[x][y][z] = 1;
-    queue<node> que;
-    que.push(e1);
+    for (int i = 0; i < 8; ++i) {
+        int next_x = x + direction[i][0];
+        int next_y = y + direction[i][1];
+        if (checkIndex(next_x, next_y) && !visited[next_x][next_y] && map[next_x][next_y] != 1) {
+            visited[x][y] = true;
 
-    while (!que.empty()) {
-        e1 = que.front();
-        que.pop();
+            search(next_x, next_y);
 
-        if (s[e1.x][e1.y][e1.z] == 'P') {
-            if (e1.step <= t) {
-                flag = 1;
-            }
-            break;
+            visited[x][y] = false;
         }
-
-        for (int i = 0; i < 4; i++) {
-            e2.x = e1.x + d[i][0];
-            e2.y = e1.y + d[i][1];
-            e2.z = e1.z;
-
-            if (e2.x >= 0 && e2.x < n && e2.y >= 0 && e2.y < m && !vis[e2.x][e2.y][e2.z] &&
-                s[e2.x][e2.y][e2.z] != '*') {
-                if (s[e2.x][e2.y][e2.z] != '#') {
-                    vis[e2.x][e2.y][e2.z] = 1;
-                    e2.step = e1.step + 1;
-                    que.push(e2);
-                } else {
-                    vis[e2.x][e2.y][e2.z] = 1;
-                    if (e1.z == 0) {
-                        e2.z = 1;
-                        if (s[e2.x][e2.y][e2.z] != '*' && s[e2.x][e2.y][e2.z] != '#') {
-                            e2.step = e1.step;
-                            vis[e2.x][e2.y][e2.z] = 1;
-                            que.push(e2);
-                        }
-                    } else {
-                        e2.z = 0;
-                        if (s[e2.x][e2.y][e2.z] != '*' && s[e2.x][e2.y][e2.z] != '#') {
-                            e2.step = e1.step;
-                            vis[e2.x][e2.y][e2.z] = 1;
-                            que.push(e2);
-                        }
-                    }
-                }
-            }
-        }
+        continue;
     }
 }
 
 int main() {
+    /*std::fstream cin("maze.in", std::ios::in);
+          std::fstream cout("maze.out", std::ios::out | std::ios::trunc);*/
 
-    cin >> tt;
+    std::cin >> n;
 
-    while (tt--) {
-        flag = 0;
-        cin >> m >> n >> t;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> s[i][j][0];
-                if (s[i][j][0] == 'S') {
-                    sx = i;
-                    sy = j;
-                    sz = 0;
-                }
-            }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cin >> map[i][j];
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> s[i][j][1];
-                if (s[i][j][1] == 'S') {
-                    sx = i;
-                    sy = j;
-                    sz = 1;
-                }
-            }
-        }
-
-        bfs(sx, sy, sz, 1);
-
-        if (flag)
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
     }
+
+    search(0, 0);
+
+    std::cout << total;
+
+    /*cin.close();
+      cout.close();*/
 
     return 0;
 }

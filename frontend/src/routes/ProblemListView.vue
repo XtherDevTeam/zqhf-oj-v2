3abedc34e6b382aed3e07a9c6bdc55fd86a3b44b
xiaokang00010 <zhouyongkang4321@gmail.com>
2022-03-11 20:53:36 +0800

    Added comment area in ProblemListView.vue.

--- frontend/src/routes/ProblemListView.vue
+++ frontend/src/routes/ProblemListView.vue
@@ -1,28 +1,32 @@
 <template>
-  <el-card shadow="hover" class="box-card">
-    <div slot="header" class="clearfix">
-      <span>{{ problem_list_content['name'] }}</span>
-    </div>
-    <span><strong>题单介绍: </strong></span><br>
-    <div style="margin: 10px auto;"></div>
-    <div class="markdown-body" id="markdownRenderedPlace" v-html="rendered_description_content"></div>
-    <div style="margin: 10px auto;"></div>
-    <el-table :data="problem_list_content['problems']" style="width: 100%">
-      <el-table-column prop="id" fixed="right" label="题目编号" width="100px"></el-table-column>
-      <el-table-column prop="name" fixed="right" label="题目标题"></el-table-column>
+  <div>
+    <el-card shadow="hover" class="box-card">
+      <div slot="header" class="clearfix">
+        <span>{{ problem_list_content['name'] }}</span>
+      </div>
+      <span><strong>题单介绍: </strong></span><br>
+      <div style="margin: 10px auto;"></div>
+      <div class="markdown-body" id="markdownRenderedPlace" v-html="rendered_description_content"></div>
+      <div style="margin: 10px auto;"></div>
+      <el-table :data="problem_list_content['problems']" style="width: 100%">
+        <el-table-column prop="id" fixed="right" label="题目编号" width="100px"></el-table-column>
+        <el-table-column prop="name" fixed="right" label="题目标题"></el-table-column>
 
-      <el-table-column fixed="right" label="操作">
-        <template v-slot="scope">
-          <el-button @click="goto_problem(scope.row.id)" type="text" size="small">查看</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </el-card>
+        <el-table-column fixed="right" label="操作">
+          <template v-slot="scope">
+            <el-button @click="goto_problem(scope.row.id)" type="text" size="small">查看</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+    <comment style="margin: 10px auto;" class="box-card" :area_id="'problem_list:' + this.problem_list_content.id"></comment>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import MonacoEditor from "../components/editor.vue";
+import Comment from "../components/comment";
 
 import markdownItHighlight from 'markdown-it-highlight';
 
@@ -31,7 +35,6 @@ const markdown = require('markdown-it')(),
 
 markdown.use(markdownItHighlight);
 markdown.use(markdown_with_katex);
-
 
 
 export default {
@@ -73,6 +76,9 @@ export default {
       problem_list_content: "",
       rendered_description_content: ""
     }
+  },
+  components: {
+    comment: Comment
   }
 };
 </script>
